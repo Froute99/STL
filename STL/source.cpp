46554e5814432615bdf657f00fb1536c439d3b3e
Froute99 <froute99619@gmail.com>
2022-05-14 11:40:58 +0900

    하는중

--- STL/source.cpp
+++ STL/source.cpp
@@ -1,52 +1,66 @@
 /*
- * 2022. 05. 11
+ * 2022. 05. 12
  * STL Class
  *
+ * Containers are objects that store other objects.
+ * 1. Sequence container
+ * 2. Associative container - set/multi_set/map/multi_map
  * 
- * map<key, value, key_compare>
+ *		map<key, value, key_compare>
  *
- * 5/25(수) - 졸업연구설명회(? 2시30)
- * 5/30 ~ 6/20
- *
+ * 5/25(수) - 졸업연구설명회(? 2시30)	지도교수 5/30 ~ 6/20
+ * 6.15 기말고사
+ * 
  */
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <map>
+#include <algorithm>
 #include "save.h"
-#include "STRING.h"
 
 extern bool isMessageToggle;
 
 
 int main() {
-	std::map<std::string, std::vector<std::string>> gameCompany{ {"넥슨", {"바람의 나라", "메이플스토리"}} };
+	// [문제] 알파벳과 빈도수를 화면에 출력하라
+	// std::map<알파벳, 개수>
 
-	std::pair<std::string, std::vector<std::string>> p{ "엔씨", {"리니지"} };
-	gameCompany.insert(p);
-	gameCompany.insert(std::make_pair("펄어비스", std::vector<std::string>{ "검은사막" }));
+	std::ifstream in{ "Frankenstein.txt" };
+	std::vector<std::string> v{ std::istream_iterator<std::string>{in}, {} };
+	std::map<char, int> alphaNum;
 
-	// 리스트 전체 출력
-	
-	for (const auto& [회사, 게임들] : gameCompany) {
-		std::cout << 회사 << " - ";
-		for (const auto& 게임 : 게임들) {
-			std::cout << 게임 << " ";
-		}
-		std::cout << std::endl;
+	for (const auto& word : v)
+		for (char letter : word)
+			if (isalpha(letter))
+				alphaNum[letter]++;
+
+
+	std::cout << "단어 갯수 - " << v.size() << std::endl;
+
+	// [문제] 이 소설에 사용된 단어와 개수를 출력하라.
+	// 앞에서부터 20개만 출력하라
+	// 개수 오름차순으로 정렬하라
+
+	std::map<std::string, int, std::greater<int>> wordNum;
+
+	for (const std::string& word : v) {
+		wordNum[word]++;
 	}
 
-	std::string company;
-	// [문제] 사용자가 입력한 회사의 게임을 출력
-	while (true) {
-		std::cout << "찾을 회사는? ";
-		std::cin >> company;
+	std::vector<std::pair<std::string, int>> civ{ wordNum.begin(), wordNum.end() };
+	std::sort(civ.begin(), civ.end(),
+		[](const auto& a, const auto& b) {
+			return a.second > b.second;
+		});
 
-		// 있으면 출력
-		// 없으면 없다고 출력
+	auto p = wordNum.begin();
+	for (int i = 0; i < 1000; ++i, ++p) {
+		std::cout << p->first << " - " << p->second << std::endl;
 	}
-	
 
-	//save("source.cpp", "STL_0511.txt");
+
+	save("source.cpp", "STL_0512.txt");
 }
 
