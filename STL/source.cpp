c0e8f1a5c1dc56ed0ea3e8e5ccca0fdbd8a119ab
Froute99 <froute99619@gmail.com>
2022-05-14 12:01:45 +0900

    끝

--- STL/source.cpp
+++ STL/source.cpp
@@ -14,53 +14,13 @@
  */
 
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <map>
-#include <algorithm>
 #include "save.h"
 
 extern bool isMessageToggle;
 
 
 int main() {
-	// [문제] 알파벳과 빈도수를 화면에 출력하라
-	// std::map<알파벳, 개수>
-
-	std::ifstream in{ "Frankenstein.txt" };
-	std::vector<std::string> v{ std::istream_iterator<std::string>{in}, {} };
-	std::map<char, int> alphaNum;
-
-	for (const auto& word : v)
-		for (char letter : word)
-			if (isalpha(letter))
-				alphaNum[letter]++;
-
-
-	std::cout << "단어 갯수 - " << v.size() << std::endl;
-
-	// [문제] 이 소설에 사용된 단어와 개수를 출력하라.
-	// 앞에서부터 20개만 출력하라
-	// 개수 오름차순으로 정렬하라
-
-	std::map<std::string, int, std::greater<int>> wordNum;
-
-	for (const std::string& word : v) {
-		wordNum[word]++;
-	}
-
-	std::vector<std::pair<std::string, int>> civ{ wordNum.begin(), wordNum.end() };
-	std::sort(civ.begin(), civ.end(),
-		[](const auto& a, const auto& b) {
-			return a.second > b.second;
-		});
-
-	auto p = wordNum.begin();
-	for (int i = 0; i < 1000; ++i, ++p) {
-		std::cout << p->first << " - " << p->second << std::endl;
-	}
-
-
+	
 	save("source.cpp", "STL_0512.txt");
 }
 
