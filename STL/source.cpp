06f5bf95226ee20ff86a5669757c25ea905b2adb
Froute99 <froute99619@gmail.com>
2022-06-04 00:57:50 +0900

    STL 시험 공부

--- STL/source.cpp
+++ STL/source.cpp
@@ -1,47 +1,42 @@
 /*
- * 2022. 05. 26
+ * 2022. 06. 02
  * STL Class
  * JH Kim
  *
- * 
+ *
  * C++20의 핵심
  * concepts
  * range/view
- *		coroutine	- 
+ *		coroutine	-
  *		module		- ixx
- * 
- * 
+ *
+ *
  * type traits
- * 
- * 
+ *
+ *
  *		6.15 기말고사
  *		6.16 강의 끝 - 한 학기 강의를 정리 책 소개
  *		6.20 점수 공개 사무실 앞
  */
 
 #include <iostream>
-#include <vector>
-#include <list>
-#include <algorithm>
-#include <concepts>
 #include <ranges>
+#include <format>
 #include "save.h"
 #include "STRING.h"
 
-extern bool isMessageToggle;
 
-
-int main() {
-
-	std::vector<int> v{ 1,2,3,4,5 };
-
-	std::ranges::sort(v, std::greater<int>(), {});
-
-	for (int n : v) {
-		std::cout << n << " ";
-	}
+int main()
+{
+	// [문제] 10000부터 시작하는 무한개의 자연수를 생성하고 홀수 2000개만 역순으로 출력하라
+	auto odd = [](int n) { return n % 2; };
+	for (int n : std::views::iota(10000) | std::views::reverse
+										 | std::views::filter(odd)
+										 | std::views::take(2000)
+										 )
+		std::cout << std::format("{:8}", n);
 	std::cout << std::endl;
 
-	save("source.cpp", "STL_0526.txt");
+	save("source.cpp", "STL_0602.txt");
 }
 
